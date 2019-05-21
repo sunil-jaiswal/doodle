a54797308fd0ecaa38e277a1c3b04e579f06f3c0
Rajneesh Sharma <Rajneesh.Sharma@healogics.com>
2019-05-21 16:22:45 +0530

    welcome1

--- src/main/java/com/doodle/controller/Welcome.java
+++ src/main/java/com/doodle/controller/Welcome.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class Welcome {
 
-	@RequestMapping(value="welcome")
+	@RequestMapping(value="welcome1")
 	public String welcome() {
 		return "dashboard";
 	}
